4bc4cead267be38c9fe487938a11816aef773459
kimrichardj@gmail.com <kimrichardj@gmail.com>
2026-03-14 19:55:56 -0700

    update

--- obj/Debug/net10.0-maccatalyst/maccatalyst-x64/AlternateRealityDungeon.AssemblyInfo.cs
+++ obj/Debug/net10.0-maccatalyst/maccatalyst-x64/AlternateRealityDungeon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlternateRealityDungeon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+850864f31df5495db1f63f60fe6aa371d5706931")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c5ac27371c12e03a3fa664d28d9b36dd7af5dab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlternateRealityDungeon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlternateRealityDungeon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
